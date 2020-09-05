fcb7473ffdeecb6e5bc22d2e31ef894bfecb929b
Rohit J <65280723+rohitj205@users.noreply.github.com>
2020-09-05 14:37:41 +0530

    Test to Add

---
+++ DICTIONARY.py
@@ -0,0 +1,7 @@
+d = {'Rob':454454565,'Mac':565454165,'John':548784555,'Sneha':545448948}
+for key in d:
+    print("Key:",key,"Value",d[key])
+
+for k,v in d.items():
+
+    print("Key:",key,"Value",v)
